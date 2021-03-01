787eaa3a85f1d7c53da8c605d20873217291724e
Cole Gawin <colegawin@gmail.com>
2021-03-01 15:36:23 -0600

    fix: move word files to public folder fix: fetch word files from internet as opposed to local

--- util/api.ts
+++ util/api.ts
@@ -1,7 +1,4 @@
-import * as fs from "fs";
-import { promisify } from "util";
-
-const random = (contents: string) => {
+const getRandomWord = (contents: string) => {
   contents = contents.replace(/[\r]/g, "");
   const words = contents.split("\n");
   words.pop();
@@ -9,25 +6,22 @@ const random = (contents: string) => {
   return words[i];
 };
 
-const getAllWordTypes = async () => {
-  const wordTypes = await promisify(fs.readdir)("db");
-  return wordTypes.map((w) => w.split(".")[0].slice(0, -1));
-};
-
-const contentsRetriever = async (filePath) => {
-  const contents = await promisify(fs.readFile)(filePath);
-  return random(contents.toString()) + " ";
+const getWordFile = async (filePath) => {
+  const contents = await fetch(
+    (process.env.NODE_ENV === "production" ? "https://words-aas.next.sh/db/" : "http://localhost:3000/") + filePath,
+  );
+  return getRandomWord(contents.toString()) + " ";
 };
 
 const phraseGenerator = async (words) => {
   let phrase = "";
-  const allWordTypes = await getAllWordTypes();
+  const allWordTypes = ["adjective", "adverb", "animal", "bodypart", "gerund", "noun", "pluralNoun", "verb"];
   for (const word of words) {
     if (word === "") continue;
 
     if (word !== "a" && allWordTypes.includes(word)) {
-      const filePath = "db/" + word + "s.txt";
-      phrase += await contentsRetriever(filePath);
+      const filePath = word + "s.txt";
+      phrase += await getWordFile(filePath);
     } else if (word !== "a") phrase += word + " ";
   }
   return phrase;
@@ -38,6 +32,6 @@ const vowelTester = (phrase) => new RegExp(/[aeiou]/gi).test(phrase[0]);
 export const phraseResolver = async (words: string[]) => {
   let phrase = await phraseGenerator(words);
   if (words[0] == "a") phrase = (vowelTester(phrase) ? "an" : "a") + " " + phrase;
-  phrase = phrase.slice(0, phrase.length - 1);
+  phrase = phrase.slice(0, -1);
   return { phrase };
 };
